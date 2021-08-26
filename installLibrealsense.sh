95b659fca7ce9eb7d2dc0b3a81c8fcca97c8ef90
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-26 15:12:11 -0700

    Update keyserver url

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -5,7 +5,7 @@
 # MIT License
 # https://github.com/IntelRealSense/librealsense/blob/master/doc/installation_jetson.md
 # Register the server's public key:
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 
+sudo apt-key adv --keyserver keyserver.ubuntu.com  --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key
 
 # Ubuntu 18 is bionic
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
