6cfba1960141b7ed7700a14fbaa860ef0803faf8
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-01 23:34:51 -0700

    Update apt url

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -8,7 +8,8 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com  --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key
 
 # Ubuntu 18 is bionic
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+# sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u # Broken? (got 403 forbidden)
+sudo add-apt-repository "deb https://librealsense.intel.com/Debian/apt-repo $(lsb_release -cs) main" -u
 
 sudo apt-get install apt-utils -y
 sudo apt-get install librealsense2-utils librealsense2-dev -y
